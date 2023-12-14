502ee4f401e49b3f11f93959e359c68bddb1c522
vasilev-g <vassilev-g3@gmail.com>
2023-12-14 15:49:31 +0200

    First Commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+
+    const handleClick = () => {
+
+    }
+
+    return ( 
+        <div className="home">
+            <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+        </div>
+     );
+}
+ 
+export default Home;
